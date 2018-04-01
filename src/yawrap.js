066dd9659fac38a1be70e7f519f8d0ba237dc235
YawZhou <admin@yawzhou.com>
2018-04-01 17:21:09 +0800

    Fix a few minor problems.

--- src/yawrap.js
+++ src/yawrap.js
@@ -10,16 +10,17 @@
  * https://opensource.org/licenses/MIT
  *
  * Start Date: 2017.09.18
- * Last Update : 2018.03.09 17:10
+ * Last Update : 2018.04.01 16:55
  */
 ;(function(window, undefined){'use strict';
     window.addEventListener("load",function(){
-        console.log("%c Yawrap v0.3.1 By YawZhou && FanyXu http://yawrap.yawzhou.com ","background:#2196f3;color:#fff;padding:2px;border-radius:20px;");
+        var projectName = 'Yawrap';
+        var version = 'v0.3.1';
         document.body.style.display = 'block';
-        var yaw = document.querySelectorAll('*');
+        var yaw = document.querySelectorAll('a,article,address,aside,audio,b,base,blockquote,body,br,button,canvas,caption,cite,code,col,colgroup,command,datalist,dd,del,details,dialog,div,dl,dt,em,embed,fieldset,figcaption,figure,font,footer,form,frame,h1,h2,h3,h4,h5,h6,header,hr,i,iframe,img,input,ins,label,li,map,mark,menu,nav,ol,option,output,p,pre,progress,q,section,select,small,source,span,strong,sub,sup,table,tbody,td,textarea,tfoot,th,thead,time,title,tr,track,tt,u,ul,var,video,wbr');
         for (var i = 0; i < yaw.length; i++) {
-            // noinspection JSAnnotator
             function yawrap(w, h, maw, mah, miw, mih, lh, fs, cl, bg, ti, t, r, b, l, mg, mgt, mgr, mgb, mgl, pd, pdt, pdr, pdb, pdl, bd, bdt, bdr, bdb, bdl, br, zi, ls) {
+            // noinspection JSAnnotator
                 yaw[i].style.width = isNaN(w) ? w : w + 'px';
                 yaw[i].style.height = isNaN(h) ? h : h + 'px';
                 yaw[i].style.maxWidth = isNaN(maw) ? maw : maw + 'px';
@@ -56,8 +57,10 @@
             }
             yawrap(yaw[i].getAttribute('w')||undefined,yaw[i].getAttribute('h')||undefined,yaw[i].getAttribute('maw')||undefined,yaw[i].getAttribute('mah')||undefined,yaw[i].getAttribute('miw')||undefined,yaw[i].getAttribute('mih')||undefined,yaw[i].getAttribute('lh')||undefined,yaw[i].getAttribute('fs')||undefined,yaw[i].getAttribute('cl')||undefined,yaw[i].getAttribute('bg')||undefined,yaw[i].getAttribute('ti')||undefined,yaw[i].getAttribute('t')||undefined,yaw[i].getAttribute('r')||undefined,yaw[i].getAttribute('b')||undefined,yaw[i].getAttribute('l')||undefined,yaw[i].getAttribute('mg')||undefined,yaw[i].getAttribute('mgt')||undefined,yaw[i].getAttribute('mgr')||undefined,yaw[i].getAttribute('mgb')||undefined,yaw[i].getAttribute('mgl')||undefined,yaw[i].getAttribute('pd')||undefined,yaw[i].getAttribute('pdt')||undefined,yaw[i].getAttribute('pdr')||undefined,yaw[i].getAttribute('pdb')||undefined,yaw[i].getAttribute('pdl')||undefined,yaw[i].getAttribute('bd')||undefined,yaw[i].getAttribute('bdt')||undefined,yaw[i].getAttribute('bdr')||undefined,yaw[i].getAttribute('bdb')||undefined,yaw[i].getAttribute('bdl')||undefined,yaw[i].getAttribute('br')||undefined,yaw[i].getAttribute('zi')||undefined,yaw[i].getAttribute('ls')||undefined);
             yaw[i].removeAttribute('w');yaw[i].removeAttribute('h');yaw[i].removeAttribute('maw');yaw[i].removeAttribute('mah');yaw[i].removeAttribute('miw');yaw[i].removeAttribute('mih');yaw[i].removeAttribute('lh');yaw[i].removeAttribute('fs');yaw[i].removeAttribute('cl');yaw[i].removeAttribute('bg');yaw[i].removeAttribute('ti');yaw[i].removeAttribute('t');yaw[i].removeAttribute('r');yaw[i].removeAttribute('b');yaw[i].removeAttribute('l');yaw[i].removeAttribute('mg');yaw[i].removeAttribute('mgt');yaw[i].removeAttribute('mgr');yaw[i].removeAttribute('mgb');yaw[i].removeAttribute('mgl');yaw[i].removeAttribute('pd');yaw[i].removeAttribute('pdt');yaw[i].removeAttribute('pdr');yaw[i].removeAttribute('pdb');yaw[i].removeAttribute('pdl');yaw[i].removeAttribute('bd');yaw[i].removeAttribute('bdt');yaw[i].removeAttribute('bdr');yaw[i].removeAttribute('bdb');yaw[i].removeAttribute('bdl');yaw[i].removeAttribute('br');yaw[i].removeAttribute('zi');yaw[i].removeAttribute('ls');
-            /* Resolve the page as soon as the problem of opening the display is out of order
-               Replace the text */
+            var yawrapURL = 'http://yawrap.yawzhou.com';
+            var author = 'YawZhou && FanyXu';
+            /* Resolve the page as soon as the problem of opening the display is out of order.
+             * Replace the text */
             var yawText = yaw[i].getAttribute('text');
             if (yawText === '') {
                 yaw[i].innerHTML = '<p style="color:red;font-size:12px"><strong>Yawrap提示：</strong>text属性值不可以为空</p>';
@@ -69,8 +72,9 @@
             var yawImg = yaw[i].getAttribute('yawsrc');
             if (yawImg != null){
                 yaw[i].setAttribute('src',yawImg);
-                yaw[i].removeAttribute('yawsec');
+                yaw[i].removeAttribute('yawsrc');
             }
+            /* Responsive */
             if (document.body.clientWidth > 1180) {
                 yawrap(yaw[i].getAttribute('pc-w')||undefined,yaw[i].getAttribute('pc-h')||undefined,yaw[i].getAttribute('pc-maw')||undefined,yaw[i].getAttribute('pc-mah')||undefined,yaw[i].getAttribute('pc-miw')||undefined,yaw[i].getAttribute('pc-mih')||undefined,yaw[i].getAttribute('pc-lh')||undefined,yaw[i].getAttribute('pc-fs')||undefined,yaw[i].getAttribute('pc-cl')||undefined,yaw[i].getAttribute('pc-bg')||undefined,yaw[i].getAttribute('pc-ti')||undefined,yaw[i].getAttribute('pc-t')||undefined,yaw[i].getAttribute('pc-r')||undefined,yaw[i].getAttribute('pc-b')||undefined,yaw[i].getAttribute('pc-l')||undefined,yaw[i].getAttribute('pc-mg')||undefined,yaw[i].getAttribute('pc-mgt')||undefined,yaw[i].getAttribute('pc-mgr')||undefined,yaw[i].getAttribute('pc-mgb')||undefined,yaw[i].getAttribute('pc-mgl')||undefined,yaw[i].getAttribute('pc-pd')||undefined,yaw[i].getAttribute('pc-pdt')||undefined,yaw[i].getAttribute('pc-pdr')||undefined,yaw[i].getAttribute('pc-pdb')||undefined,yaw[i].getAttribute('pc-pdl')||undefined,yaw[i].getAttribute('pc-bd')||undefined,yaw[i].getAttribute('pc-bdt')||undefined,yaw[i].getAttribute('pc-bdr')||undefined,yaw[i].getAttribute('pc-bdb')||undefined,yaw[i].getAttribute('pc-bdl')||undefined,yaw[i].getAttribute('pc-br')||undefined,yaw[i].getAttribute('pc-zi')||undefined,yaw[i].getAttribute('pc-ls')||undefined);
                 yaw[i].removeAttribute('pc-w');yaw[i].removeAttribute('pc-h');yaw[i].removeAttribute('pc-maw');yaw[i].removeAttribute('pc-mah');yaw[i].removeAttribute('pc-miw');yaw[i].removeAttribute('pc-mih');yaw[i].removeAttribute('pc-lh');yaw[i].removeAttribute('pc-fs');yaw[i].removeAttribute('pc-cl');yaw[i].removeAttribute('pc-bg');yaw[i].removeAttribute('pc-ti');yaw[i].removeAttribute('pc-t');yaw[i].removeAttribute('pc-r');yaw[i].removeAttribute('pc-b');yaw[i].removeAttribute('pc-l');yaw[i].removeAttribute('pc-mg');yaw[i].removeAttribute('pc-mgt');yaw[i].removeAttribute('pc-mgr');yaw[i].removeAttribute('pc-mgb');yaw[i].removeAttribute('pc-mgl');yaw[i].removeAttribute('pc-pd');yaw[i].removeAttribute('pc-pdt');yaw[i].removeAttribute('pc-pdr');yaw[i].removeAttribute('pc-pdb');yaw[i].removeAttribute('pc-pdl');yaw[i].removeAttribute('pc-bd');yaw[i].removeAttribute('pc-bdt');yaw[i].removeAttribute('pc-bdr');yaw[i].removeAttribute('pc-bdb');yaw[i].removeAttribute('pc-bdl');yaw[i].removeAttribute('pc-br');yaw[i].removeAttribute('pc-zi');yaw[i].removeAttribute('pc-ls');
@@ -82,6 +86,7 @@
                 yaw[i].removeAttribute('m-w');yaw[i].removeAttribute('m-h');yaw[i].removeAttribute('m-maw');yaw[i].removeAttribute('m-mah');yaw[i].removeAttribute('m-miw');yaw[i].removeAttribute('m-mih');yaw[i].removeAttribute('m-lh');yaw[i].removeAttribute('m-fs');yaw[i].removeAttribute('m-cl');yaw[i].removeAttribute('m-bg');yaw[i].removeAttribute('m-ti');yaw[i].removeAttribute('m-t');yaw[i].removeAttribute('m-r');yaw[i].removeAttribute('m-b');yaw[i].removeAttribute('m-l');yaw[i].removeAttribute('m-mg');yaw[i].removeAttribute('m-mgt');yaw[i].removeAttribute('m-mgr');yaw[i].removeAttribute('m-mgb');yaw[i].removeAttribute('m-mgl');yaw[i].removeAttribute('m-pd');yaw[i].removeAttribute('m-pdt');yaw[i].removeAttribute('m-pdr');yaw[i].removeAttribute('m-pdb');yaw[i].removeAttribute('m-pdl');yaw[i].removeAttribute('m-bd');yaw[i].removeAttribute('m-bdt');yaw[i].removeAttribute('m-bdr');yaw[i].removeAttribute('m-bdb');yaw[i].removeAttribute('m-bdl');yaw[i].removeAttribute('m-br');yaw[i].removeAttribute('m-zi');yaw[i].removeAttribute('m-ls');
             }
         }
+        window.console.log("%c " + projectName + " " + version + " " + author + " " + yawrapURL + " ", "background:#2196f3;color:#fff;padding:2px;border-radius:20px;");
     },true);
 })(window);
 //Define your website's primary and secondary colors as well as secondary colors
